8e6ab44f518d059ff2f38f4c2c75ec2336053903
Bo Feng <bfeng1216@gmail.com>
2012-03-31 07:16:15 -0500

    improve gantt chart

--- js/main.js
+++ js/main.js
@@ -25,42 +25,57 @@
         }
     });
 
-/*
     var GanttChart = Backbone.View.extend({
         el: $('#gantt-chart'),
         initialize: function() {
             _.bindAll(this, 'render');
-            this.render();
         },
 
         render: function() {
-            var html = '<table class="table"></table>';
-            $(this.el).append(html);
+            $(this.el).empty();
+            var progress = $('<div class="progress">');
+            progress.append($('<div class="bar" style="width: 60%">'));
+            var template = $('<table class="table table-bordered">');
+            template.append($('<tr>').append('<th>Title</th>').append('<th>Timeline</th>'));
+            for(var i=0;i<10;i++) {
+                var row = $('<tr>').append('<td width="30%"><span class="badge">'+i+'</span></td>');
+                var line = $('<td width="70%">');
+                
+                var boxes = $('<table width="100%" boder="0">').append($('<tr>'));
+                
+                for(var j=0;j<10;j++) {
+                    if(j==i) {
+                        var color = '#'+Math.floor(Math.random()*(0xFFFFFF+1)<<0).toString(16);
+                        boxes.append('<td boder="0" width="10%" style="background-color:'+color+';border:none"> </td>');
+                    } else {
+                        boxes.append('<td boder="0" width="10%" style="border:none;"> </td>');
+                    }
+                }
+
+                line.append(boxes);
+                row.append(line);
+                template.append(row);
+            }
+            $(this.el).append(progress).append(template);
         }
     });
-*/
+
+    var ganttChart = new GanttChart();
 
     var navBar = new NavBar();
-    var listView = new ListView();
-
-    jQuery("#gantt-chart").gantt({
-        source: "js/data.json",
-        itemsPerPage: 50,
-        months: [
-            "January", "February", "March",
-            "April", "May", "June", "July",
-            "August", "September", "October",
-            "November", "December"],
-        dow: ["S", "M", "T", "W", "Th", "F", "Sa"],
-        navigate: 'scroll',
-        scale: 'hours',
-        maxScale: 'days',
-        minScale: 'hours',
-        holidays: [
-            "\/Date(1293836400000)\/",
-            "\/Date(1351724400000)\/"
-        ]
-    });
+    //var listView = new ListView();
 
     $('.dropdown-toggle').dropdown(); 
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "gantt":"ganttChartShow"
+        },
+        ganttChartShow: function() {
+            ganttChart.render();
+        }
+    });
+
+    var app_router = new AppRouter;
+    Backbone.history.start();
 })(jQuery);
