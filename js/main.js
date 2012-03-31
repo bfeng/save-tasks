5960aabc6421f3e240e1c644cc29da42a440418a
Bo Feng <bfeng1216@gmail.com>
2012-03-30 23:55:13 -0500

    dropdown javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 (function($){
-/*
-    var ListView = Backbone.View.extend({
+    var NavBar = Backbone.View.extend({
         el: $('body'),
         initialize: function() {
             _.bindAll(this, 'render');
@@ -8,10 +7,24 @@
         },
         
         render: function() {
-            $(this.el).append("<h1>hello</h1>");
+            $(this.el).append('');
         }
     });
 
+    var ListView = Backbone.View.extend({
+        el: $('#listView'),
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.render();
+        },
+        
+        render: function() {
+            $(this.el).append('<div style="padding: 8px 0;" class="well"><ul class="nav nav-list"></ul></div>');
+        }
+    });
+
+    var navBar = new NavBar();
     var listView = new ListView();
-*/
+
+    $('.dropdown-toggle').dropdown(); 
 })(jQuery);
