e252a2a07b11ac03a2ba5f4a3309e2b86a4d3e6f
Bo Feng <bfeng1216@gmail.com>
2012-03-31 05:08:41 -0500

    add models

--- addtask.py
+++ addtask.py
@@ -4,6 +4,8 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+from models import Task
+
 class AddTaskPage(webapp.RequestHandler):
     def get(self):
         self.response.out.write("...")
