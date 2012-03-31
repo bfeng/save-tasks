24e2d8f8b476572971d73864419f469020097639
mcheung <mcheung2@example.com>
2012-03-31 05:13:48 -0500

    addtask button

--- addtask.py
+++ addtask.py
@@ -6,4 +6,8 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class AddTaskPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("...")
+        #<input name="subscribe" type="checkbox" value="yes />
+        subscribe_to_newsletter = self.request.get("subscribe", default_value="no")
+        path = os.path.join(os.path.dirname(__file__), 'addtask.html')
+        template_values = {}
+        self.response.out.write(template.render(path, template_values))
